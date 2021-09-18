1242041e89f6ca03da7eb1ddd61e9b947a9639de
Jakub Prymak <jprymak@gmail.com>
2021-09-18 16:54:27 +0200

    refactor: change SelectionInfo to render content dynamically

--- src/Components/SelectionInfo.jsx
+++ src/Components/SelectionInfo.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
+
 export function SelectionInfo({
-    pipe, velocity, pressureDrop
+  ...props
 }) {
   return (
     <div className="selection-info">
-      <p className="selection-info__item"> Selected pipe: <strong>DN {pipe}</strong></p>
-      <p className="selection-info__item"> Velocity: {velocity} m/s</p>
-      <p className="selection-info__item"> Pressure drop: {pressureDrop} Pa/m</p>
+      {
+        Object.keys(props).map(key => {
+          switch (key) {
+            case "pipe": return <React.Fragment key={key}><p className="selection-info__item"> Selected pipe: <strong>DN {props[key]}</strong></p></React.Fragment >
+            case "flow": return <React.Fragment key={key}><p className="selection-info__item"> Flow: {props[key]} m3/h</p></React.Fragment >
+            case "velocity": return <React.Fragment key={key}><p className="selection-info__item"> Velocity: {props[key]} m/s</p></React.Fragment>
+            case "pressureDrop": return <React.Fragment key={key}><p className="selection-info__item"> Pressure drop: {props[key]} Pa/m</p></React.Fragment>
+            default: return null
+          }
+        })
+      }
     </div>
   );
 }
