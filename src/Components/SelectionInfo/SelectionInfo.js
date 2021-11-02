9fbe9f6f0241123bbf7e9da9d47f5596b0b17d51
Jakub Prymak <jprymak@gmail.com>
2021-11-02 21:01:27 +0100

    refactor: remove expendable React.Fragment

--- src/Components/SelectionInfo/SelectionInfo.js
+++ src/Components/SelectionInfo/SelectionInfo.js
@@ -10,10 +10,10 @@ export default function SelectionInfo({
       {
         Object.keys(infoProps).map(key => {
           switch (key) {
-            case "pipe": return <React.Fragment key={key}><p className="selection-info__item"> Selected pipe: <strong>DN {infoProps[key]}</strong></p></React.Fragment >;
-            case "flow": return <React.Fragment key={key}><p className="selection-info__item"> Flow: {infoProps[key]} m3/h</p></React.Fragment >;
-            case "velocity": return <React.Fragment key={key}><p className="selection-info__item"> Velocity: {infoProps[key]} m/s</p></React.Fragment>;
-            case "pressureDrop": return <React.Fragment key={key}><p className="selection-info__item"> Pressure drop: {infoProps[key]} Pa/m</p></React.Fragment>;
+            case "pipe": return <p key={key} className="selection-info__item"> Selected pipe: <strong>DN {infoProps[key]}</strong></p>;
+            case "flow": return<p key={key} className="selection-info__item"> Flow: {infoProps[key]} m3/h</p>;
+            case "velocity": return <p key={key} className="selection-info__item"> Velocity: {infoProps[key]} m/s</p>;
+            case "pressureDrop": return <p key={key} className="selection-info__item"> Pressure drop: {infoProps[key]} Pa/m</p>;
             default: return null;
           }
         })
