ecee558e7c4eb8ec4ad1270562bda0a2a0a6247b
Jakub Prymak <jprymak@gmail.com>
2021-11-03 18:41:31 +0100

    refactor: create a renderSwitch for inputs in FastCalc component

--- src/Pages/FastCalc.js
+++ src/Pages/FastCalc.js
@@ -4,23 +4,23 @@ import { useGlobalContext } from "../context";
 
 import { Form } from "../Components/Form";
 import { SelectionInfo } from "../Components/SelectionInfo";
+import { Input } from "../Components/Input";
 
 import { useEffect, useReducer } from "react";
 import { stateReducer } from "../Hooks/stateReducer";
 import getDispatchObj from "../Data/dispatchObj";
 import initialState from "../Data/initialState";
 
-export default function FastCalc({info, inputs}) {
-const {closeSubmenu} = useGlobalContext();
-const [state, dispatch] = useReducer(stateReducer, initialState);
+export default function FastCalc({ info, inputs }) {
+  const { closeSubmenu } = useGlobalContext();
+  const [state, dispatch] = useReducer(stateReducer, initialState);
 
+  useEffect(() => {
+    dispatch({ type: "setStateToInitial", payload: initialState });
+    dispatch({ type: "setPipe" });
+  }, [info, inputs]);
 
-useEffect(()=>{
-  dispatch({type: "setStateToInitial", payload: initialState});
-  dispatch({type: "setPipe"});
-  },[info, inputs]);
-
-const convertArrayToObject = (array, source) => {
+  const convertArrayToObject = (array, source) => {
     return array.reduce((obj, item) => {
       return {
         ...obj,
@@ -33,15 +33,94 @@ const convertArrayToObject = (array, source) => {
     dispatch(getDispatchObj(e));
   };
 
+  const convertedInputs = convertArrayToObject(inputs, state);
+  const convertedInfo = convertArrayToObject(info, state);
+
+  const inputRenderSwitch = (key) => {
+    switch (key) {
+      case "flow":
+        return (
+          <Input
+            key={key}
+            name="flow-input"
+            label="Flow"
+            onInputChange={handleInputChange}
+            type="number"
+            min={0}
+            value={convertedInputs[key]}
+            unit="m3/h"
+          />
+        );
+
+      case "allowedVelocity":
+        return (
+          <Input
+            key={key}
+            name="allowed-velocity-input"
+            label={`Allowed velocity (${convertedInputs[key]})`}
+            onInputChange={handleInputChange}
+            type="range"
+            min={1}
+            max={3}
+            step="0.1"
+            value={convertedInputs[key]}
+            unit="m/s"
+          />
+        );
+
+      case "allowedPressureDrop":
+        return (
+          <Input
+            key={key}
+            name="allowed-pressure-drop-input"
+            label={`Allowed pressure drop (${convertedInputs[key]})`}
+            onInputChange={handleInputChange}
+            type="range"
+            min={100}
+            max={1000}
+            step="50"
+            value={convertedInputs[key]}
+            unit="Pa/m"
+          />
+        );
+
+      case "delta":
+        return (
+          <Input
+            key={key}
+            name="delta-input"
+            label={`Delta (${convertedInputs[key]})`}
+            onInputChange={handleInputChange}
+            type="number"
+            min={0}
+            max={50}
+            value={convertedInputs[key]}
+            unit="K"
+          />
+        );
+
+      case "capacity":
+        return (
+          <Input
+            key={key}
+            name="capacity-input"
+            label={`Delta (${convertedInputs[key]})`}
+            onInputChange={handleInputChange}
+            type="number"
+            min={0}
+            value={convertedInputs[key]}
+            unit="kW"
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="mode" onMouseOver={closeSubmenu}>
-      <SelectionInfo
-      infoProps={convertArrayToObject(info, state)}
-      />
-      <Form
-        onInputChange={handleInputChange}
-        inputs={convertArrayToObject(inputs, state)}
-      />
+      <SelectionInfo infoProps={convertedInfo} />
+      <Form>{inputs.map((key) => inputRenderSwitch(key))}</Form>
     </div>
   );
 }
