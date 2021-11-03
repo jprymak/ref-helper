9f64d801300f8c55cf0cad1ac89d9bb81d04d33b
Jakub Prymak <jprymak@gmail.com>
2021-11-03 18:40:10 +0100

    refactor: create a separate component for inputs

---
+++ src/Components/Input/Input.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Input({name, label, onInputChange, type, min, max, value, unit, step}) {
+    return (
+        <>
+            <label htmlFor={name}>{label}:</label>
+                <input
+                  onChange={onInputChange}
+                  id={name}
+                  name={name}
+                  type={type}
+                  min={min}
+                  max={max}
+                  value={value}
+                  step={step}
+                />
+                <span>{unit}</span>
+        </>
+    );
+}
+
+export default Input;
