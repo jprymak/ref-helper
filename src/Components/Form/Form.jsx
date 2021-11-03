d097b79e348f1f44adf45b6ac08cdc5c7ecb5a30
Jakub Prymak <jprymak@gmail.com>
2021-11-03 18:39:05 +0100

    refactor: change form into a dumb component

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,98 +2,10 @@ import React from "react";
 
 import "./index.scss";
 
-export default function Form({ inputs, onInputChange }) {
+export default function Form({children}) {
   return (
-    <form onSubmit={e=>e.preventDefault()} className="form">
-      {Object.keys(inputs).map((key) => {
-        switch (key) {
-          case "flow":
-            return (
-              <React.Fragment key={key}>
-                <label htmlFor="flow-input">Flow: </label>
-                <input
-                  onChange={onInputChange}
-                  name="flow-input"
-                  type="number"
-                  min="0"
-                  value={inputs[key]}
-                />
-                <span>m3/h</span>
-              </React.Fragment>
-            );
-          case "allowedVelocity":
-            return (
-              <React.Fragment key={key}>
-                <label htmlFor="allowed-velocity-input">
-                  Allowed velocity ({inputs[key]}):{" "}
-                </label>
-                <input
-                  onChange={onInputChange}
-                  name="allowed-velocity-input"
-                  value={inputs[key]}
-                  type="range"
-                  step="0.1"
-                  min="1"
-                  max="3"
-                />
-                <span>m/s</span>
-              </React.Fragment>
-            );
-
-          case "allowedPressureDrop":
-            return (
-              <React.Fragment key={key}>
-                <label htmlFor="allowed-pressure-drop-input">
-                  Allowed pressure drop ({inputs[key]}):{" "}
-                </label>
-                <input
-                  onChange={onInputChange}
-                  name="allowed-pressure-drop-input"
-                  value={inputs[key]}
-                  type="range"
-                  step="50"
-                  min="100"
-                  max="1000"
-                />
-                <span>Pa/m</span>
-              </React.Fragment>
-            );
-
-            case "delta":
-            return (
-              <React.Fragment key={key}>
-                <label htmlFor="delta-input">
-                  Delta ({inputs[key]}):{" "}
-                </label>
-                <input
-                  onChange={onInputChange}
-                  name="delta-input"
-                  type="number"
-                  min="0"
-                  max="50"
-                  value={inputs[key]}
-                />
-                <span>K</span>
-              </React.Fragment>
-            );
-
-            case "capacity":
-              return (
-                <React.Fragment key={key}>
-                  <label htmlFor="capacity-input">Capacity: </label>
-                  <input
-                    onChange={onInputChange}
-                    name="capacity-input"
-                    type="number"
-                    min="0"
-                    value={inputs[key]}
-                  />
-                  <span>kW</span>
-                </React.Fragment>
-              );
-              default: return null;
-        }
-      })}
+    <form onSubmit={(e) => e.preventDefault()} className="form">
+      {children}
     </form>
   );
 }
