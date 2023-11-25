b1b2d5f900179a3ffb15c992a10f731b88d9ffd5
Jakub Prymak <jprymak@gmail.com>
2023-11-25 12:20:25 +0100

    remove unused cra features && add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "/ref-helper",
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
